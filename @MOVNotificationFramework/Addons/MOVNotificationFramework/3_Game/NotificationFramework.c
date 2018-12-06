31a83cb94b08b7894bc5c983cea99714644fb3b9
mov3ax <35667100+mov3ax@users.noreply.github.com>
2018-12-06 21:34:39 +0200

    Beta 0.47

--- @MOVNotificationFramework/Addons/MOVNotificationFramework/3_Game/NotificationFramework.c
+++ @MOVNotificationFramework/Addons/MOVNotificationFramework/3_Game/NotificationFramework.c
@@ -15,15 +15,7 @@ class NotificationMeta
 	
 	void NotificationMeta()
 	{
-		this.m_Queue = GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY); 
-		
-		this.m_Alert = GetGame().GetWorkspace().CreateWidgets( "mov/scripts/layouts/alert.layout" );
-		this.m_AlertIcon = ImageWidget.Cast( this.m_Alert.FindAnyWidget("NotificationImage") );
-		this.m_AlertText = TextWidget.Cast( this.m_Alert.FindAnyWidget("NotificationText") );
-		
-		this.m_Alert.Show(false);
-		this.m_AlertIcon.LoadImageFile(0, "mov/scripts/images/alert.paa");
-		this.m_AlertText.SetText("");		
+				
 	}
 }
 
@@ -37,6 +29,16 @@ class NotificationFramework
 		GetRPCManager().AddRPC( "MOVNotificationFramework", "HideFunction", this, SingeplayerExecutionType.Client );
 		
 		m_Notification = new NotificationMeta();
+		
+		m_Notification.m_Queue = GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY); 
+		
+		m_Notification.m_Alert = GetGame().GetWorkspace().CreateWidgets( "mov/scripts/layouts/alert.layout" );
+		m_Notification.m_AlertIcon = ImageWidget.Cast( m_Notification.m_Alert.FindAnyWidget("NotificationImage") );
+		m_Notification.m_AlertText = TextWidget.Cast( m_Notification.m_Alert.FindAnyWidget("NotificationText") );
+		
+		m_Notification.m_Alert.Show(false);
+		m_Notification.m_AlertIcon.LoadImageFile(0, "mov/scripts/images/alert.paa");
+		m_Notification.m_AlertText.SetText("");
 	}
 	
 	void ShowFunction( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target )
