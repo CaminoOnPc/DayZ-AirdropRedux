f44c7cb6e048d564c5f5be52c8efcf20f79f6626
mov3ax <35667100+mov3ax@users.noreply.github.com>
2018-12-06 15:54:31 +0200

    Beta 0.31

---
+++ @MOV_Airdrop/mod.cpp
@@ -0,0 +1,29 @@
+class CfgMods
+{
+	class MOV_Airdrop
+	{
+	    dir = "MOV_Airdrop";
+	    picture = "";
+	    action = "";
+	    hideName = 1;
+	    hidePicture = 1;
+	    name = "AirDrop Redux";
+	    credits = "mov3ax, philipp";
+	    author = "mov3ax, philipp";
+	    authorID = "0";
+	    version = "1.0";
+	    extra = 0;
+	    type = "mod";
+
+	    dependencies[] = {"Mission", "gui"};
+
+	    class defs
+	    {
+			class missionScriptModule
+            {
+                value = "";
+                files[] = {"mov/framework/notification/scripts/5_Mission"};
+            };
+        };
+    };
+};
