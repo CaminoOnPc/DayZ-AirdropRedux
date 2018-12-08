56644d14bd7f3c890dddd509f373265b5fb26c73
mov3ax <35667100+mov3ax@users.noreply.github.com>
2018-12-08 18:33:58 +0200

    Beta 0.65

--- @MOVAirdrop/Addons/MOVAirdrop/5_Mission/AirdropInit.c
+++ @MOVAirdrop/Addons/MOVAirdrop/5_Mission/AirdropInit.c
@@ -1,25 +1,17 @@
 modded class MissionServer
 {
-	ref AirDrop_Base m_Airdrop;
-	
     void MissionServer()
     {
-		Print("<AirDrop> MissionServer::MissionServer()");
-		
-		m_Airdrop = null;
-        m_Airdrop = new AirDrop_Base();
+		g_AirdropBase = null;
+        GetAirdropBase();
     }
 }
 
 modded class MissionGameplay
 {
-	ref AirDrop_Base m_Airdrop;
-	
     void MissionGameplay()
     {
-		Print("<AirDrop> MissionGameplay::MissionGameplay()");
-		
-		m_Airdrop = null;
-        m_Airdrop = new AirDrop_Base();
+		g_AirdropBase = null;
+        GetAirdropBase();
     }
 }
