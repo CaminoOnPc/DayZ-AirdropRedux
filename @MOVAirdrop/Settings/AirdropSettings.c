e9763d42c4f8a442a08093d2697f464d422f5901
mov3ax <35667100+mov3ax@users.noreply.github.com>
2018-12-09 20:58:22 +0200

    Release 1.0

--- @MOVAirdrop/Settings/AirdropSettings.c
+++ @MOVAirdrop/Settings/AirdropSettings.c
@@ -15,7 +15,6 @@ float m_Initial = 0.01; // Time in mutires how ofter airdrop will spawn
 int m_Items = 7; // Items count in airdrop
 int m_Infected = 15; // Infected count around airdrop
 	
-float m_FallSpeed = 0.2; // Airdrop fall speed in meters per second
 float m_Speed = 10.0; // Airplane speed in meters per second
 float m_Height = 300; // Airplane fly height above surface
 	
